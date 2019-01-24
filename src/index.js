732dc09fcc1d037b7be33f79fbeb8807523348bb
Konstantin Lobov <Lobov.K.Evg@sberbank.ru>
2019-01-24 13:54:55 +0300

    Replaced counter with news loading component

--- src/index.js
+++ src/index.js
@@ -5,17 +5,35 @@ import Button from '@material-ui/core/Button'
 import Counter from './counter'
 
 class App extends React.Component {
+
+    state = {
+        articles: []
+    }
+
+    loadNews = () => {
+        fetch('https://meduza.io/api/v3/search?chrono=news&locale=ru&page=0&per_page=24')
+            .then(response => {
+                return response.json()
+            })
+            .then((data) => {
+                this.setState({articles: Object.values(data.documents)})
+            })
+    }
+
     render() {
         return (
             <div>
-               <Counter initialCount={5} />
-               <Counter initialCount={-8} />
-                <Button onClick={()=>{}}>
-                    Добавить счётчик
+                <Button onClick={this.loadNews}>
+                    Загрузить новости
                 </Button>
+                <ul>
+                    {this.state.articles.map(article => (
+                        <li key={article.title}>{article.title}</li>
+                    ))}
+                </ul>
             </div>
         )
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App/>, document.getElementById('app'))
